7f51e4722a0d2d715d22426aeda0b0ed10d5f8df
robmainah <44176348+robmainah@users.noreply.github.com>
2023-08-18 16:32:40 +0300

    nuxt navigate to

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -1,12 +1,23 @@
+<script lang="ts" setup>
+const city = ref<string>("");
+const cityError = ref<boolean>(false);
+// const router = useRouter();
+
+const handleSearch = () => {
+  if (!city.value) {
+    return cityError.value = true;
+  }
+
+  navigateTo(`/city/${city.value}/car`)
+  // router.push(`/city/${city.value}/car`)
+}
+</script>
 <template>
-    <div
-      class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
-    >
-      <input
-        type="text"
-        class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
-        placeholder="Search by city..."
-      />
-      <button class="bg-sky-500 px-10 text-white">Search</button>
-    </div>
+  <div
+    class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto"
+    :class="cityError && 'border border-red-500'">
+    <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city..."
+      v-model="city" @keyup.enter="handleSearch" />
+    <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
+  </div>
 </template>
