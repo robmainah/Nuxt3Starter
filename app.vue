c13134f51e7ba3450afbe96117918f811258ce38
robmainah <44176348+robmainah@users.noreply.github.com>
2023-08-18 16:19:43 +0300

    add pages

--- app.vue
+++ app.vue
@@ -1,3 +1,6 @@
 <template>
-  <div></div>
+  <div>
+    <Navbar />
+    <NuxtPage />
+  </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <CarHero />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
