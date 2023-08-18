d1e9d658569ebacfc10b4bddf269498af37436bb
robmainah <44176348+robmainah@users.noreply.github.com>
2023-08-18 17:38:00 +0300

    add layouts

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 const route = useRoute();
-
 useHead({
   title: route.params.name.toString().toUpperCase(),
+});
+
+definePageMeta({
+  layout: 'custom'
 })
 </script>
 
 <template>
-  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+  <div>
     <CarDetailHero />
     <CarDetailAttributes />
     <CarDetailDescription />
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -3,14 +3,16 @@ const route = useRoute();
 
 useHead({
   title: `${route.params.make ? route.params.make.toString() : 'Cars'} in ${route.params.city.toString().toUpperCase()}`,
+});
+
+definePageMeta({
+  layout: 'custom',
 })
 </script>
 
 <template>
-  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-    <div class="mt-32 flex">
-      <CarSidebar />
-      <NuxtPage />
-    </div>
+  <div class="mt-32 flex">
+    <CarSidebar />
+    <NuxtPage />
   </div>
 </template>
